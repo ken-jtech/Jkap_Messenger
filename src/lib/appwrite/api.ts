2463b2163061516e637a9a8e08d402493acb12ba
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2023-12-20 14:37:16 -0500

    updated commits

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,9 +1,9 @@
 import { INewUser } from "@/types";
-import { ID } from 'appwrite';
+import { ID, Query } from 'appwrite';
 import { account, appwriteConfig, avatars, databases } from "./config";
 import { error } from "console";
 
-export async function CreateUserAccount(user: INewUser) {
+export async function createUserAccount(user: INewUser) {
     try {
         const newAccount = await account.create(
             ID.unique(),
@@ -53,4 +53,40 @@ export async function saveUserToDB(user: {
         console.log(error);
     }
     
+}
+
+export async function signInAccount(user: {
+    email: string;
+    password: string
+}) {
+   try {
+    const session = await account.createEmailSession(user.email, user.password);
+    return session;
+
+   } catch (error) {
+        console.log(error);
+   } 
+}
+
+export async function getCurrentUser() {
+    try {
+        const currentAccount = await account.get();
+
+        if(!currentAccount) throw Error;
+
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [
+               Query.equal('accountId', currentAccount.$id)
+            ],
+        )
+
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+
+    } catch (error) {
+        console.log(error);
+    }
 }
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -4,11 +4,19 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { CreateUserAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
-export const useCreateUserAccountMutation = () => {
+export const useCreateUserAccount = () => {
     return useMutation({
-        mutationFn: (user: INewUser) => CreateUserAccount(user)
-    })
+        mutationFn: (user: INewUser) => createUserAccount(user),
+    });
+}
+    export const useSignInAccount = () => {
+        return useMutation({
+            mutationFn: (user: { 
+                email: string;
+                password: string
+            }) => signInAccount(user),
+        })
 }
